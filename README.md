0d91dd7b4a1f616ef7246068e51a7ebc88ccb987
maitag <info@maitag.de>
2025-12-28 00:06:42 +0100

    macro to switch between ViktorT and ViktorInt and to wrap basic types by Null<>

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ haxelib git viktor https://github.com/maitag/viktor.git
 
 - synopsis
 - new types what not indicate values by `null` or `-1` if they gets deleted (on cost of iteration speed)
-- a macro for Victor to choose between the types: `ViktorT<T>, ViktorInt` and the not-null new ones
 - more backends what using Bytes instead of Vector
 - unit tests, benchmarks, hxp, optimization
   
--- src/Viktor.hx
+++ src/Viktor.hx
@@ -1,214 +1,61 @@
 package;
 
-import haxe.ds.Vector; // Viktors little friend .)
-
+#if !macro
 /**
-	This datastructure is to store values mapped by a integer key.
-	Its optimized for fast add/delete operations by the key.
-	Operations like remove(), indexOf() or iteration over key/values is slow instead.
+	Switches between ViktorInt, ViktorT<Null<T>> and ViktorT<T>:
+		Viktor<Int>  -> ViktorInt
+		Viktor<UInt> -> ViktorInt
+		Viktor<Bool> -> ViktorT<Null<Bool>>
+		Viktor<Float> -> ViktorT<Null<Float>>
+	all others will become ViktorT<T>!
 **/
-class Viktor<T> {
+@:genericBuild(Viktor.ViktorMacro.build())
+class Viktor<T> {}
 
-	var list:Vector<T>;
-	var pos:Int = 0;
-	
-	var freeKeys:Vector<Int>;
-	var posFree:Int = -1;
+#else
 
-	/**
-		Maximum number of values what can be stored inside.
-	**/
-	public var size(get, never):Int;
-	inline function get_size():Int return freeKeys.length;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+import haxe.macro.TypeTools;
 
-	/**
-		How many values are actually stored.
-	**/
-	public var length(get, never):Int;
-	inline function get_length():Int return pos - (posFree + 1);
+class ViktorMacro
+{
+	static public function build()
+	{	
+		// trace(Context.getLocalType());
+		switch (Context.getLocalType())
+		{
+			case TInst(_, typeParams):
 
-	/**
-		Creates a new Viktor instance.
-		@param size maximum size (greatest key will be size-1)
-	**/
-	public inline function new(size:Int) {
-		list = new Vector(size);
-		freeKeys = new Vector(size);
-	}
+				if (typeParams.length > 1) Context.error("Only one Type parameter expected", Context.currentPos());
+				var type = typeParams[0];
+				switch (type) 
+				{
+					case TAbstract(n,[]):
+						var name = n.get().name;
+						if ( name == "Int" || name == "UInt")
+						{	
+							// trace("using ViktorInt");
+							return TPath({ pack:[], name:"ViktorInt", params:[] });
+						}
+						else if (name == "Float" || name == "Bool")
+						{	
+							// trace('using Viktor<Null<$name>>');
+							return TPath({ pack:[], name:"ViktorT", params:[
+								TPType( TPath({ pack:[], name:"Null", params:[TPType(TypeTools.toComplexType(type))] }) )
+							]});
+						}
 
-	/**
-		Get the value to a key or returns `null` if not found.
-		@param key integer key
-	**/
-	public inline function get(key:Int):T {
-		return list.get(key);
-	}
-
-	/**
-		Sets the value to a key.
-		@param key integer key
-		@param key value
-		@param checkValidKey true by default, disable this for an unsafe operation, e.g. to fast replace a value
-	**/
-	public inline function set(key:Int, value:T, checkValidKey:Bool = true) {
-		if (checkValidKey) {
-			if (key < 0 || key > pos || pos == size) throw("OutOfRange");
-			if (key == pos) pos++;
-			else if (posFree >= 0) { // check if it is inside freeKeys and remove it there
-				var i:Int = 0;
-				while (i <= posFree && freeKeys.get(i) != key) i++;
-				if (i <= posFree) {
-					if (i < posFree) freeKeys.set( i, freeKeys.get(posFree) );
-					posFree--;
+					default:
 				}
-			}
+				
+				// trace('using Viktor<T>');
+				return TPath({ pack:[], name:"ViktorT", params:[TPType(TypeTools.toComplexType(type))] });
+			
+			default: Context.error("Type parameter expected", Context.currentPos());
 		}
-		list.set(key, value);
-	}
-
-	/**
-		Adds a new value and returns a new available key.
-		@param value value to add
-		@throws Overflow if there is no more free space
-		@returns key where the value is mapped to
-	**/
-	public inline function add(value:T):Int {
-		if (posFree == -1) {
-			if (pos == size) throw("Overflow");
-			list.set(pos, value);
-			return pos++;
-		}
-		else {
-			var key = freeKeys.get(posFree--);
-			list.set(key, value);
-			return key;
-		}
-	}
-
-	/**
-		Returns true if a value to the key exists.
-		@param key integer key
-	**/
-	public inline function exist(key:Int):Bool {
-		return (get(key) != null);
-	}
-
-	/**
-		Deletes the value by its key (frees the key for re-usage).
-		@param key integer key
-	**/
-	public inline function del(key:Int) {
-		// if (key < 0 || key >= pos) throw("OutOfRange");
-		list.set(key, null);
-
-		if (key == pos-1) {
-			pos--;
-		}
-		else {
-			// if (posFree >= freeKeys.length) throw("'del' freeKeys OVERFLOW");
-			freeKeys.set(++posFree, key);
-		}
-	}
-	
-	/**
-		Deletes a value if found and returns its key.
-		If more then one of same values exists it returs the one with the higher key value.
-		@param value value to delete
-	**/
-	public inline function remove(value:T):Int {
-		var i:Int = key(value);
-		if (i >= 0) del(i);
-		return i;
-	}
-
-	/**
-		Returns the key of the first value what is found or `-1` instead.
-		@param value value to get key for
-	**/
-	public inline function key(value:T):Int {
-		var i:Int = 0;		
-		while ( i < pos && get(i) != value) i++;
-		return (i<pos) ? i : -1;
-	}
-
-
-	// ------------------- ITERATORS ---------------------
-
-	/**
-		Returns a new ViktorIterator to use in `for (value in viktor)` loops.
-	**/
-	public inline function iterator():ViktorIterator<T> {
-		return new ViktorIterator<T>(this, 0, pos);
-	}
-
-	/**
-		Returns a new ViktorKeyValueIterator to use in `for (value in viktor)` loops.
-	**/
-	public inline function keyValueIterator():ViktorKeyValueIterator<T> {
-		return new ViktorKeyValueIterator<T>(this, 0, pos);
+		return null;
 	}
 
 }
-
-
-// ---------------------------------------------------
-// ------------------- ITERATORS ---------------------
-// ---------------------------------------------------
-
-class ViktorIterator<T> {
-
-	var viktor:Viktor<T>;
-	var i:Int;
-	var to:Int;
-
-	/**
-		Creates a new `ViktorIterator<T>` instance.
-		@param viktor viktor reference
-		@param from iteration start value
-		@param to iteration end value
-	**/
-	public inline function new(viktor:Viktor<T>, from:Int, to:Int) {
-		if (from < 0 || from >= to || to > viktor.size) throw("Iterator out of bounds");
-		this.viktor = viktor;
-		i = from;
-		this.to = to;
-	}
-
-	public inline function next():T {
-		var v:T = viktor.get(i++);
-		while ( v == null) v = viktor.get(i++);
-		return v;
-	}
-
-	@:access(Viktor)
-	public inline function hasNext():Bool return (i < to && viktor.posFree < viktor.size);
-}
-
-class ViktorKeyValueIterator<T> {
-
-	var viktor:Viktor<T>;
-	var i:Int;
-	var to:Int;
-
-	/**
-		Creates a new `ViktorKeyValueIterator<T>` instance.
-		@param viktor viktor reference
-		@param from iteration start value
-		@param to iteration end value
-	**/
-	public inline function new(viktor:Viktor<T>, from:Int, to:Int) {
-		if (from < 0 || from >= to || to > viktor.size) throw("Iterator out of bounds");
-		this.viktor = viktor;
-		i = from;
-		this.to = to;
-	}
-
-	public inline function next():{key:Int, value:T} {
-		var v:T = viktor.get(i++);
-		while ( v == null) v = viktor.get(i++);
-		return {key:i-1, value:v};
-	}
-
-	@:access(Viktor)
-	public inline function hasNext():Bool return (i < to && viktor.posFree < viktor.size);
-}
+#end
